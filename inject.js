7be62657bb893416a9e553217a2053b01f1b6fb9
Roman Sorin <988244@students.mentorschools.org>
2019-10-15 10:07:33 -0400

    Getting scroll working

--- inject.js
+++ inject.js
@@ -1,7 +1,7 @@
 // this is the code which will be injected into a given page...
 
 ;(function () {
-  const FILTERS = ['russia', 'ukraine', 'brexit', 'biden', 'nazi']
+  const FILTERS = ['russia', 'ukraine', 'brexit', 'biden', 'nazi', 'hong kong']
   const SIDEBAR_LABEL = 'Timeline: Trending now'
 
   const trendingDiv = document.querySelector(`[aria-label="${SIDEBAR_LABEL}"]`)
@@ -9,11 +9,15 @@
 
   if (trendingDiv !== null) trendingDiv.remove()
 
-  for (const tweet of tweets) {
-    FILTERS.some(filter => {
-      if (tweet.innerText.toLowerCase().includes(filter)) {
-        tweet.remove()
+  window.addEventListener('scroll', function () {
+    window.requestAnimationFrame(function () {
+      for (const tweet of tweets) {
+        FILTERS.some(filter => {
+          if (tweet.innerText.toLowerCase().includes(filter)) {
+            tweet.remove()
+          }
+        })
       }
     })
-  }
+  })
 })()
