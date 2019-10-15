4f83159d77ef26e070937df4ee17e58b5f515905
Roman Sorin <988244@students.mentorschools.org>
2019-10-15 09:26:14 -0400

    Filtering

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
 // this is the background code...
 
 // listen for our browerAction to be clicked
-chrome.tabs.onUpdated.addListener(function (tab) {
+chrome.tabs.onUpdated.addListener(function(tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.ib, {
 		file: 'inject.js'
 	});
 });
+
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+})
--- inject.js
+++ inject.js
@@ -1,14 +1,21 @@
 // this is the code which will be injected into a given page...
 
 (function() {
+    const FILTERS = ['russia', 'ukraine']
 
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
+    const trending_div = document.querySelector('[aria-label="Timeline: Trending now"]')
+    const tweets = document.querySelectorAll('article')
+
+    if (trending_div !== null)
+        trending_div.remove()
+   
+
+    for (const tweet of tweets) {
+        FILTERS.some(filter=> {
+          
+            tweet.innerText.includes(filter); tweet.remove()
+            })
+    }
 
 
 })();
